b149fbe635fc1f05cd4f14208e30c5bfa2c47ca6
Truperton <58820346+Truperton@users.noreply.github.com>
2024-09-27 17:39:34 +0100

    Completed "Balloon Popper - Part 3" of the course.
    As part of this lesson I have created a prefab(scene) of the balloon so we can instantiate it better. Also created the balloon manager script which keeps track of the score.

---
+++ Balloon Popper/Scripts/balloon manager.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var score : int = 0
+
+func increase_score(amount):
+	score += amount
+	print(score)
--- Balloon Popper/Scripts/balloon.gd
+++ Balloon Popper/Scripts/balloon.gd
@@ -1,12 +1,16 @@
 extends Area3D
 
 var clicks_to_pop : int = 3
-var size_incerase : float = 0.2
+var size_increase : float = 0.2
 var score_to_give : int = 1
 
 
 func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
 		print("Increase Size")
+		scale += Vector3.ONE * size_increase
+		clicks_to_pop -= 1
 		
-		pass 
+		if clicks_to_pop <= 0:
+			get_node("/root/Root").increase_score(score_to_give)
+			queue_free()
