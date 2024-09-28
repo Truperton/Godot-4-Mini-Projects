6090b437fd4cf6504ed5cb585b7a045664480f26
Truperton <58820346+Truperton@users.noreply.github.com>
2024-09-28 18:46:27 +0100

    Completed "Loops - Part 2" of the course
    For this I created a script that instantiates a desired number of "stars" on the screen. One big change I went with over the lesson, was I added a texture rect for the black background instead of changing the editor's background.

--- Loops/Scripts/loops.gd
+++ Loops/Scripts/loops.gd
@@ -1,6 +1,17 @@
 extends Node2D
 
+@export var spawn_count : int = 200
+@export var star_scene = preload("res://Loops/Prefabs/Star.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	for i in spawn_count:
+		var star = star_scene.instantiate()
+		add_child(star)
+		
+		star.position.x = randf_range(-580, 580)
+		star.position.y = randf_range(-330, 330)
+		
+		var star_scale : float = randf_range(0.5, 1)
+		star.scale.x = star_scale
+		star.scale.y = star_scale
